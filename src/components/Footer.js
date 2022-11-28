c3dff55ccd778963da607374aa6a1e4f6d8a0fd0
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2022-11-28 20:55:11 -0300

    finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { ReactComponent as GitHub } from "../assets/github.svg";
+import { ReactComponent as LinkedIn } from "../assets/linkedin.svg";
 
 const Footer = () => {
-  return <div className={`${styles.footer} container`}>Footer</div>;
+  return (
+    <div className={`${styles.footer} container`}>
+      Desenvolvido por Gustavo Junqueira Barbosa -
+      <a href="https://github.com/gustavojb1" target="_blank" rel="noreferrer">
+        <GitHub className={styles.logo} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/gustavo-barbosa-b2668023a/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedIn className={styles.logo} />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
